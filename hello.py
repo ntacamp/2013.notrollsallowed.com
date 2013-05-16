30e4bb16f65d1ca5b2672ae417e59c79dd4299c2
Povilas Balzaravicius Pawka <pavvka@gmail.com>
2013-05-16 13:56:01 +0300

    Added IRC channel

--- hello.py
+++ hello.py
@@ -5,4 +5,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return '<p>Hello Trolls!</p><p><a href="https://groups.google.com/forum/?fromgroups=#!forum/no-trolls-allowed">Join our mailing group @ Google Groups</p>'
+    return '<p>Hello Trolls!</p><p><a href="https://groups.google.com/forum/?fromgroups=#!forum/no-trolls-allowed">Join our mailing group @ Google Groups</p><p>IRC: #parallax-manufactory @ Freenode</p>'

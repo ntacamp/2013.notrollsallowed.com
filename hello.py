6bdec0264776a397e73406285b05bf02386bf107
Povilas Balzaravicius Pawka <pavvka@gmail.com>
2013-05-16 14:08:27 +0300

    Added Twitter link.

--- hello.py
+++ hello.py
@@ -5,4 +5,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return '<p>Hello Trolls!</p><p><a href="https://groups.google.com/forum/?fromgroups=#!forum/no-trolls-allowed">Join our mailing group @ Google Groups</a></p><p>IRC: #parallax-manufactory @ Freenode</p>'
+    return '<p>Hello Trolls!</p><p><a href="https://groups.google.com/forum/?fromgroups=#!forum/no-trolls-allowed">Join our mailing group @ Google Groups</a></p><p>IRC: #parallax-manufactory @ Freenode</p><p>Twitter: <a href="http://www.twitter.com/ntacamp">@ntacamp</a></p>'
